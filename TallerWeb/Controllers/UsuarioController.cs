bb7dbf93a35906f89a47b9a1b38c4a23a69d6733
abrahamendez89_ <abrahamendez89@hotmail.com>
2015-11-24 17:52:32 -0700

    primer version

---
+++ TallerWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TallerWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //servidor/Usuario
+        // Vista para ingresar a la aplicación
+        public ActionResult Ingresar()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TallerWeb/Frontend/testJQuery.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(loadEventos);
+
+function loadEventos() {
+    $("#btn_agregarEmpleado").on('click', function () {
+
+        //var color = 'red';
+        //if ($("#top_bar").css("background-color") == 'rgb(255, 0, 0)') {
+        //    color = 'blue'
+        //}
+
+        //$("#top_bar").css({
+        //    "background-color": "" + color + "",
+        //    "color": "blue"
+        //});
+
+        //$("#top_bar").addClass("menu_rojo");
+
+        //alert('algo');
+
+    });
+
+    $("#btn_menu").on('click', function () {
+
+        if ($("#menu").hasClass('mostrarMenu')) {
+            $("#menu").removeClass("mostrarMenu");
+            $("#top_bar").removeClass("moverTopBar");
+            return;
+        }
+        $("#menu").addClass("mostrarMenu");
+        $("#top_bar").addClass("moverTopBar");
+
+    });
+
+    $(".btn.btn-toolbar").on('click', function () {
+        var li = '<li id="btn_agregarEmpleado" class="hvr-bounce-in">'
+                + '<span class="icono-menu icon-users"></span>'
+                + '<span class="titulo-menu">Otra opcion</span>'
+           + '</li>';
+
+        $("#menu ul").append(li);
+
+        mensaje.notificacion("hola", {_titulo:'PROBLEMAS!!'} ,  function () {
+            //alert("hola callback");
+        });
+
+    });
+
+    $("#menu ul li").on('click', function () {
+        
+    });
+}
